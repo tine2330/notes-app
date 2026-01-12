83c1ec5b63cd0de15321ba8cd87139b4e4d2cda8
Kristine Mae De La Cruz <delacruzkristine23aa@gmail.com>
2026-01-13 00:52:23 +0800

    Initial commit: App.js working and project setup

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Notes App</Text>
+      <Text>React Native + Supabase</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
